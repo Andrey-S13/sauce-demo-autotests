ea8208ec83ca8e35c03af0c88d2eb6f2c51dc5e7
Andrey-S13 <suvorike13@gmail.com>
2026-02-06 02:40:22 +0300

    Добавление авторизации,формирование ID, добавление товара в корзину и проверки на добавление

--- Задание 3 по smoke test с любым товаром.py
+++ Задание 3 по smoke test с любым товаром.py
@@ -1,38 +1,10 @@
 import time
+
 from selenium import webdriver  # инструмент для автоматизации браузера
 from selenium.webdriver.common.by import By  # поиск элементов на странице
-from selenium.webdriver.common import keys  # инструменты взаимодействия с элементами на странице
 from selenium.webdriver.support.wait import WebDriverWait  # явное ожидание элементов
 from selenium.webdriver.support import expected_conditions as EC  # условное ожидание элементов
 from selenium.webdriver.chrome.options import Options  # настройки браузера Chrome
-
-
-# def product_search():
-#     """Поиск товара по введенному ключу"""
-#
-#     print("Приветствую тебя в нашем интернет - магазине")
-#
-#     # создаем новый список для перечисления в print
-#     item_list = []
-#     for key, value in products.items():
-#         item_list.append(f"{key} - {value}")
-#
-#     print(f"Выбери один из следующих товаров и укажи его номер: {', '.join(item_list)}")
-#
-#     try:
-#         key = int(input())
-#
-#         if key in products:
-#             print(f"Выбран товар: {products[key]} ")
-#             print("-" * 20)  # разделитель текста (здесь и далее)
-#         else:
-#             print("Товар не выбран или не найден")
-#             print("-" * 20)
-#
-#     except ValueError:
-#         print("Ошибка: введите число!")
-#         print("-" * 20)
-
 
 
 class SauceDemoSmokeTest:
@@ -48,15 +20,6 @@ class SauceDemoSmokeTest:
         6: "Test.allTheThings() T-Shirt (Red)"
     }
 
-    """
-    Добавить методы для преобразования словаря в lowercase + "-" + фразы
-    
-    add-to-cart-sauce-labs-backpack - значение ID для добавления в корзину
-    remove-sauce-labs-backpack -  # значение ID для добавления в корзину
-    """
-
-
-
     def __init__(self):
         """
         Конструктор класса. Вызывается при СОЗДАНИИ объекта
@@ -66,31 +29,57 @@ class SauceDemoSmokeTest:
         self.selected_product_key = None  # хранение выбранного товара (1-6)
         self.selected_product_name = None  # хранение выбранного товара (имя)
 
+    @classmethod  # декоратор для обозначения метода класса, а не объекта
+    def select_product(cls):  # ссылка на сам класс
+        """
+        класс-метод можно вызвать БЕЗ создания объекта
+        """
+        print("Приветствую тебя в нашем интернет - магазине.")
+        print(f"Выбери один из следующих товаров и укажи его номер: ")
 
+        for key, value in cls.products.items():  # обращаемся к словарю класса (не объекта)
+            print(f"{key} - {value}")  # показываем товар "1..", "2.." и т.д.
 
+        while True:  # бесконечный цикл, пока не поймаем число
+            try:
+                product_key = int(input("Введите номер товара от 1 до 6: "))
 
+                if product_key in cls.products:
+                    product_name = cls.products[product_key]
 
-    # ++++++++++++++++++++++++++++++++
-    # ________________________________
+                    print(f"Выбран товар: {product_key} - {product_name}")
+                    print("-" * 20)
 
-    # ++++++++++++++++++++++++++++++++
-    # ________________________________
+                    return product_key, product_name
 
+                else:
+                    print("Ошибка выбора номера товара")
+
+            except ValueError:
+                print("Ошибка ввода")
 
     def setup_browser(self):
         """Настройка браузера"""
         chrome_options = Options()
         chrome_options.add_argument('--incognito')  # нет всплывающих окон "пароля" / "перевода стр"
+
+        # Создаем драйвер (открытие браузера с выбранными настройками)
         self.driver = webdriver.Chrome(options=chrome_options)
+
+        # Открываем сайт
         url_authorisation = "https://www.saucedemo.com/"
         self.driver.get(url_authorisation)
-        self.driver.implicitly_wait(5)
-        self.driver.maximize_window()
+
+        # Настройки ожидания
+        self.driver.implicitly_wait(5)  # ждем элементы до 5 секунд
+        self.driver.maximize_window()  # на весь экран
+
+        print("Настройка браузера инициализирована")
+
         return self.driver
 
     def authorization(self):
         """Авторизация пользователя"""
-        # Креды пользователя
         login = "standard_user"
         password = "secret_sauce"
 
@@ -115,87 +104,77 @@ class SauceDemoSmokeTest:
         url_inventory = "https://www.saucedemo.com/inventory.html"
         get_url = self.driver.current_url
         print("Current url: " + get_url)
+
         assert get_url == url_inventory
         print("Авторизованы >>> переход на страницу с товарами")
+        print("-" * 20)  # разделитель текста
 
         return self
-#
-# '''Выбор товара'''
-#
-# # словарь для подстановки ID в XPATH
-# items_id = {
-#     1: "add-to-cart-sauce-labs-backpack",
-#     2: "add-to-cart-sauce-labs-bike-light",
-#     3: "add-to-cart-sauce-labs-bolt-t-shirt",
-#     4: "add-to-cart-sauce-labs-fleece-jacket",
-#     5: "add-to-cart-sauce-labs-onesie",
-#     6: "add-to-cart-test.allthethings()-t-shirt-(red)"
-# }
-#
-# # Поиск ID в локатор XPATH по выбранному ранее товару
-# product_key = int(product)  # дубль переменной с выбранным ранее товаром
-# if product_key in items_id:
-#     ID = items_id[product_key]
-#     print(f"Выбран товар: {products.get(product_key)}")
-#     print(f"ID XPATH: {ID}")
-#     print("-" * 20)
-#
-# # Добавление товара в корзину
-# button_add_item = driver.find_element(By.XPATH, f"//button[@id='{ID}']")
-# button_add_item.click()
-# print(f"Добавлено в корзину: {ID}")
-# print("-" * 20)  # разделитель текста
-# time.sleep(2)
-#
-#
-# class ContinueSmokeTestPurchaseItem:
-#     """Создание класса для повторяющихся механик после добавления товара в корзину"""
-#
-#     def __init__(self, driver, product_key, products, items_id):  # Осознанный костыль по использованию словарей вне Класса!!!
-#         self.driver = driver  # возможность поиска элементов на странице
-#         self.product_key = product_key
-#         self.products = products
-#         self.items_id = items_id
-#
-#     def get_product_name_in_cart_format(self):
-#         """Получить имя товара в формате, используемом в корзине"""
-#         # Получаем название товара из словаря
-#         product_name = self.items_id[self.product_key]
-#         # Преобразуем в формат, используемый в ID (для возможного использования)
-#         formatted_name = product_name.replace("add-to-cart-", "").replace("s"[0], "S").replace("-", "")
-#         return formatted_name
-#
-#
-#     # def check_product_in_cart(self):
-#     #     """Проверка добавления нужного товара в корзину"""
-#     #     try:
-#     #         # Первая проверка - кнопка Remove
-#     #         ID_button_remove = self.get_remove_id()
-#     #         print(f"ID: {ID_button_remove}")
-#     #
-#     #         # Локатор кнопки Remove
-#     #         remove_button = WebDriverWait(self.driver, 10).until(
-#     #             EC.visibility_of_element_located(By.XPATH, f"//button[@id='{ID_button_remove}']")
-#     #         )
-#     #
-#     #         # Проверяем текст кнопки
-#     #         actual_text = remove_button.text
-#     #         assert actual_text == "Remove"
-#     #         print(f"Отображается 'Remove' для товара '{self.products[self.product_key]}'")
-#     #
-#     #         # Вторая проверка - счетчик корзины
-#     #         cart_badge = WebDriverWait(self.driver, 10).until(
-#     #             EC.visibility_of_element_located(By.CLASS_NAME, "shopping_cart_badge")
-#     #         )
-#     #         actual_badge_text = cart_badge.text
-#     #         assert actual_badge_text == "1"
-#     #         print(f"Значок корзины +{cart_badge.text}")
-#     #
-#     #         return True
-#     #
-#     #     except:
-#     #         print("Ошибка при добавлении товара")
-#     #         return False
+
+    def add_product_in_cart(self):
+        """
+        1. Составление ID локатора для поиска элемента "add to cart" (добавить в корзину):
+            1.1 Пробелы заменены на "-"
+            1.2 Регистр lowercase
+            1.3 Добавляется конструкция "add-to-cart-"
+
+        2 Составление ID локатора для поиска элемента "remove-" (удалить из корзины):
+            2.1 [см 1.1...]
+            2.2 [см 1.2...]
+            2.3 Добавляется конструкция "remove-"
+
+        3. Добавление товара в корзину
+        4. Проверки:
+            4.1 Смена кнопки на Remove
+            4.2 Счетчик корзины +1
+        """
+        # ID локатор "add-to-cart-"
+        ID_add_product = self.products[self.selected_product_key]
+        ID_add = f"add-to-cart-{ID_add_product.lower().replace(" ", "-")}"
+        print(f"Отладка. Локатор ID_add: {ID_add}")
+
+        # ID локатор "remove-"
+        ID_add_product = self.products[self.selected_product_key]
+        ID_remove = f"remove-{ID_add_product.lower().replace(" ", "-")}"
+        print(f"Отладка. Локатор ID_remove: {ID_remove}")
+
+        # Добавление товара в корзину
+        button_add_item = self.driver.find_element(By.XPATH, f"//button[@id='{ID_add}']")
+        button_add_item.click()
+        print(f"Товар '{ID_add_product}' добавлен в корзину")
+        print("-" * 20)  # разделитель текста
+        time.sleep(5)
+
+        # Проверки
+        try:
+            # Cмена названия кнопки на Remove
+            remove_button = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.ID, ID_remove))
+            )
+            # Проверяем текст кнопки
+            actual_text_remove_button = remove_button.text
+            assert actual_text_remove_button == "Remove"
+            print(f"1. Отображается 'Remove' для товара '{product_name}'")
+
+            # Счетчик корзины
+            cart_badge = WebDriverWait(self.driver, 10).until(
+                EC.visibility_of_element_located((By.CLASS_NAME, "shopping_cart_badge"))
+            )
+            # Проверяем текст счетчика
+            actual_badge_text = cart_badge.text
+            assert actual_badge_text == "1"
+            print(f"2. Значок корзины +{cart_badge.text}")
+
+            return True
+
+        except:
+            print("Ошибка при добавлении товара")
+            return False
+
+
+
+
+
 #
 #     def check_product_in_cart(self):
 #         """Проверка добавления нужного товара в корзину с отладкой"""
@@ -299,8 +278,8 @@ class SauceDemoSmokeTest:
 print("Запуск автоматизированного тестирования Sauce Demo")
 print("-" * 20)
 
-# Создаем экземпляр класса
-test = SauceDemoSmokeTest(product_search)
+
+
 # test.run()  # Вызываем основной метод, который вызывает все остальные
 # )
 #
@@ -315,16 +294,35 @@ test = SauceDemoSmokeTest(product_search)
 # print("=" * 60)
 
 
+if __name__ == "__main__":
+    """
+    Запускаем код напрямую из файла или python current_name_file.py 
+    Если через импорт, программа не запустится!
+    """
+    # Выбираем товар (класс.метод)
+    product_key, product_name = SauceDemoSmokeTest.select_product()
 
-"""
-1. Запускаем программу
-2. Программа спрашивает: "Какой товар тестируем? (1-6)"
-3. Пользователь вводит число (например, 3)
-4. Программа запоминает: "тестируем товар №3"
-5. Открывается браузер
-6. Автоматически вводится логин/пароль
-7. Автоматически добавляется товар №3 в корзину
-8. Проверяется, что товар добавился
-9. Браузер закрывается
-10. Программа говорит: "Тест пройден!" или "Ошибка!"
-"""
+    # создаем объект (объект.метод)
+    test = SauceDemoSmokeTest()
+    result_settings = test.setup_browser()
+    result_authorization = test.authorization()
+    # сохраняем значение переменной в атрибуте объекта (чтобы не падала ошибка KeyError: None)
+    test.selected_product_key = product_key
+    result_add_product = test.add_product_in_cart()
+
+
+
+
+
+    """
+    1. Запускаем программу
+    2. Программа спрашивает: "Какой товар тестируем? (1-6)"
+    3. Пользователь вводит число (например, 3)
+    4. Программа запоминает: "тестируем товар №3"
+    5. Открывается браузер
+    6. Автоматически вводится логин/пароль
+    7. Автоматически добавляется товар №3 в корзину
+    8. Проверяется, что товар добавился
+    9. Браузер закрывается
+    10. Программа говорит: "Тест пройден!" или "Ошибка!"
+    """
