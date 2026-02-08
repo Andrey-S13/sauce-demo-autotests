f52152c13ab920d1c0df2089484c3a1da8e4ddc6
Andrey-S13 <suvorike13@gmail.com>
2026-02-09 00:05:29 +0300

    Задание 3 - доработка и отладка

--- Задание 3 по smoke test с любым товаром.py
+++ Задание 3 по smoke test с любым товаром.py
@@ -32,17 +32,18 @@ class SauceDemoSmokeTest:
     @classmethod  # декоратор для обозначения метода класса, а не объекта
     def select_product(cls):  # ссылка на сам класс
         """
-        класс-метод можно вызвать БЕЗ создания объекта
+        класс-метод можно вызвать БЕЗ создания объекта. Т.е. сначала отработает захардкоженная часть по заданию
         """
+        print("-" * 20)  # разделитель текста
         print("Приветствую тебя в нашем интернет - магазине.")
-        print(f"Выбери один из следующих товаров и укажи его номер: ")
+        print(f"Выбери один из следующих товаров и укажи его номер: \n")
 
         for key, value in cls.products.items():  # обращаемся к словарю класса (не объекта)
             print(f"{key} - {value}")  # показываем товар "1..", "2.." и т.д.
 
         while True:  # бесконечный цикл, пока не поймаем число
             try:
-                product_key = int(input("Введите номер товара от 1 до 6: "))
+                product_key = int(input("\nВведите номер товара от 1 до 6: "))
 
                 if product_key in cls.products:
                     product_name = cls.products[product_key]
@@ -56,7 +57,7 @@ class SauceDemoSmokeTest:
                     print("Ошибка выбора номера товара")
 
             except ValueError:
-                print("Ошибка ввода")
+                print("Ошибка ввода: недопустимый символ")
 
     def setup_browser(self):
         """Настройка браузера"""
@@ -123,9 +124,10 @@ class SauceDemoSmokeTest:
             2.3 Добавляется конструкция "remove-"
 
         3. Добавление товара в корзину
-        4. Проверки:
-            4.1 Смена кнопки на Remove
-            4.2 Счетчик корзины +1
+        4. Определение цены товара
+        5. Проверки:
+            5.1 Смена кнопки на Remove
+            5.2 Счетчик корзины +1
         """
         # ID локатор "add-to-cart-"
         ID_add_product = self.products[self.selected_product_key]
@@ -141,15 +143,24 @@ class SauceDemoSmokeTest:
         button_add_item = self.driver.find_element(By.XPATH, f"//button[@id='{ID_add}']")
         button_add_item.click()
         print(f"Товар '{ID_add_product}' добавлен в корзину")
+
+        # цена товара на витрине (для дальнейших проверок в корзине и на финально странице)
+        price_inventory = self.driver.find_element(By.XPATH, f"(//div[@class='inventory_item_price'])[{product_key}]")
+        self.price_inventory_text = price_inventory.text
+        print(f"Цена товара на главной странице: {self.price_inventory_text}")
         print("-" * 20)  # разделитель текста
         time.sleep(2)
 
         # Проверки
         try:
             # Cмена названия кнопки на Remove
-
-            # двойные скобки, чтобы система не думала, что у нас два аргумента
-            # ((внешние скобки - вызов метода, внутренние - один кортеж-аргумент))
+            """          
+            двойные скобки, чтобы система не думала, что у нас два аргумента
+            ((внешние скобки - вызов метода, внутренние - один кортеж-аргумент))
+            (
+                EC.visibility_of_element_located((By.ID, ID_remove))
+            )
+            """
             remove_button = WebDriverWait(self.driver, 10).until(
                 EC.visibility_of_element_located((By.ID, ID_remove))
             )
@@ -166,6 +177,7 @@ class SauceDemoSmokeTest:
             actual_badge_text = cart_badge.text
             assert actual_badge_text == "1"
             print(f"2. Значок корзины +{cart_badge.text}")
+            print("-" * 20)  # разделитель текста
 
             return True
 
@@ -200,19 +212,14 @@ class SauceDemoSmokeTest:
 
             # цена товара в корзине
             value_xpath = "//*[@id='cart_contents_container']/div/div[1]/div[3]/div[2]/div[2]/div"
-            price_cart_inventory_one = self.driver.find_element(By.XPATH, {value_xpath})
-
-            # цена товара на витрине
-            price_number = product_key
-            inventory_item_price = self.driver.find_element(By.CLASS_NAME, "inventory_item_price[price_number]")
-            value_price_inventory = inventory_item_price.text
+            price_cart_inventory = self.driver.find_element(By.XPATH, f"{value_xpath}")
+            print(f"Цена в корзине: {price_cart_inventory.text}")
 
             # сверка цены
-            value_price_cart_inventory_one = price_cart_inventory_one.text
-            assert value_price_cart_inventory_one == value_price_inventory
-            print(value_price_cart_inventory_one)
-
-            print(f"Цена товара в корзине корректна: {value_price_cart_inventory_one}")
+            value_price_cart_inventory = price_cart_inventory.text
+            assert value_price_cart_inventory == self.price_inventory_text
+            print(f"Цена товара в корзине не отличается")
+            print("-" * 20)  # разделитель текста
 
             return True
 
@@ -220,6 +227,119 @@ class SauceDemoSmokeTest:
             print(f"Ошибка при проверке корзины: {e}")
             return False
 
+    def checkout_page(self):
+        """Страница просмотра выбранных товаров"""
+        checkout_button = self.driver.find_element(By.CSS_SELECTOR, ".btn.btn_action.btn_medium.checkout_button")
+        checkout_button.click()
+        time.sleep(1)
+        print("Отладка. Нажатие на 'Checkout'")
+
+        url_information_page = "https://www.saucedemo.com/checkout-step-one.html"
+        current_url_after_checkout = self.driver.current_url
+        assert current_url_after_checkout == url_information_page
+        print(f"Current url: {url_information_page}")
+
+    def add_personal_data_page(self):
+        """Заполнение данных для оформления заказа"""
+        # имя
+        first_name = self.driver.find_element(By.ID, "first-name")
+        first_name.send_keys("Andrey")
+        # фамилия
+        last_name = self.driver.find_element(By.ID, "last-name")
+        last_name.send_keys("Suvorov")
+        # почтовый индекс
+        Postal_code = self.driver.find_element(By.ID, "postal-code")
+        Postal_code.send_keys("123456")
+        time.sleep(1)
+        print("Контактные данные заполнены")
+
+    def click_continue(self):
+        """подтвердить заполнение персональных данных"""
+        Continue_button = self.driver.find_element(By.ID, "continue")
+        Continue_button.click()
+        time.sleep(1)
+        print("Кнопка 'Continue' нажата")
+
+        url_overview_page = "https://www.saucedemo.com/checkout-step-two.html"
+        current_url_after_continue = self.driver.current_url
+        assert current_url_after_continue == url_overview_page
+        print("Переход на страницу 'Checkout: Overview' выполнен")
+        print("-" * 20)  # разделитель текста
+        time.sleep(2)
+
+    def check_overview_page(self):
+        """проверка заказа"""
+        # Сравниваем по товарам на витрине
+        overview_inventory = self.driver.find_element(By.CLASS_NAME, 'inventory_item_name')
+        name_overview_inventory = overview_inventory.text
+        assert name_overview_inventory == self.selected_product_name
+        print(f"Название товара идентично другим вкладкам ({name_overview_inventory})")
+        # Сравниваем по товарам в корзине
+        price_overview_inventory = self.driver.find_element(By.XPATH, "//*[@id='checkout_summary_container']/div/div[1]/div[3]/div[2]/div[2]/div")
+        value_price_overview_inventory = price_overview_inventory.text
+        assert value_price_overview_inventory == self.price_inventory_text
+        print(f"Цена идентична другим вкладкам ({value_price_overview_inventory})")
+
+    def check_total_price(self):
+        """Сверка финальной цены после автоматических расчетов"""
+        # преобразование стоимости в тип float
+        value_price_overview_inventory_int = self.price_inventory_text.replace('$', '')  # цена без $
+        value_price_overview = float(value_price_overview_inventory_int)
+        print("Отладка. Цена товара: " + str(value_price_overview))
+
+        # итоговая стоимость в разделе "Price Total"
+        item_total_price = self.driver.find_element(By.CLASS_NAME, "summary_subtotal_label")
+        value_item_total = item_total_price.text
+        value_item_total_number = value_item_total.replace('Item total: $', '')
+        print("Отладка. Цена всех товаров: " + value_item_total_number)
+        # налоги в разделе "Price Total"
+        item_total_tax = self.driver.find_element(By.CLASS_NAME, "summary_tax_label")
+        value_item_tax = item_total_tax.text
+        value_item_tax_number = value_item_tax.replace('Tax: $', '')
+        print("Отладка. Налог: " + value_item_tax_number)
+
+        # сравнение итоговой цены заказа с карточкой товара
+        assert float(value_price_overview) == float(value_item_total_number)
+        print("Итоговая стоимость товара правильная: " + str(value_item_total_number))
+
+        # подсчет окончательной стоимости заказа
+        total_price_order = self.driver.find_element(By.CLASS_NAME, "summary_total_label")
+        valut_total_price_order = total_price_order.text
+        finish_price_order_number = valut_total_price_order.replace('Total: $', '')
+        # сумма стоимости товара и налога
+        formula_finish_price = float(value_item_total_number) + float(value_item_tax_number)
+
+        assert formula_finish_price == float(finish_price_order_number)
+        print("Итоговая цена правильная: " + value_item_total_number + " + " +
+              value_item_tax_number + " = " + finish_price_order_number)
+
+    def finish_order_click(self):
+        """закрыть страницу с заказом"""
+        finish_button = self.driver.find_element(By.ID, "finish")
+        finish_button.click()
+        print("Кнопка 'finish' нажата")
+        print("-" * 20)  # разделитель текста
+        time.sleep(2)
+
+        complete_message = self.driver.find_element(By.XPATH, "//h2[text()='Thank you for your order!']")
+        current_massage = complete_message.text
+        assert current_massage == "Thank you for your order!"
+        time.sleep(2)
+        print("Сообщение 'Thank you for your order!'")
+        print("-" * 20)  # разделитель текста
+
+    def turn_back_home_page(self):
+        """переход обратно на домашнюю страницу"""
+        back_home_button = self.driver.find_element(By.ID, "back-to-products")
+        back_home_button.click()
+        page_url = "https://www.saucedemo.com/inventory.html"
+        current_page_url = self.driver.current_url
+        assert page_url == current_page_url
+        time.sleep(2)
+        print("Домашняя страница")
+
+        print("=" * 20)  # разделитель текста
+        print("Smoke тест пройден")
 
 
 if __name__ == "__main__":
@@ -237,22 +357,14 @@ if __name__ == "__main__":
     # сохраняем значение переменной в атрибуте объекта (чтобы не падала ошибка KeyError: None)
     test.selected_product_key = product_key
     test.selected_product_name = product_name
-    result_add_product = test.add_product_in_cart()
-    result_verify_cart = test.verify_product_in_cart()
 
-
-
-
-
-    """
-    1. Запускаем программу
-    2. Программа спрашивает: "Какой товар тестируем? (1-6)"
-    3. Пользователь вводит число (например, 3)
-    4. Программа запоминает: "тестируем товар №3"
-    5. Открывается браузер
-    6. Автоматически вводится логин/пароль
-    7. Автоматически добавляется товар №3 в корзину
-    8. Проверяется, что товар добавился
-    9. Браузер закрывается
-    10. Программа говорит: "Тест пройден!" или "Ошибка!"
-    """
+    # Запуск методов (тесты)
+    test.add_product_in_cart()
+    test.verify_product_in_cart()
+    test.checkout_page()
+    test.add_personal_data_page()
+    test.click_continue()
+    test.check_overview_page()
+    test.check_total_price()
+    test.finish_order_click()
+    test.turn_back_home_page()
